d2eae894ffee579db64a835d4befe0dd82e19270
Jenkin <zhaojiancun@yunquna.com>
2022-09-19 20:06:40 +0800

    chore: add spring test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,71 @@
+import { animated, useSpring, transitions, useTransition } from '@react-spring/web';
 import Toggle from './Toggle';
-// import Portal from './Portal';
-import Modal from './Modal';
+// import Modal from './Modal';
+// import User from './User';
+import UserProvider from './UserProvider';
+
+const FadeIn = ({ visible, children }) => {
+
+    const transitions = useTransition(visible, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+        delay: 200,
+        // onRest: () => setItems([]),
+      })
+
+
+    // const styles = useTransition({
+    //     from: {
+    //         opacity: 0,
+    //     },
+    //     to: {
+    //         opacity: 1,
+    //     },
+    //     leave: {
+    //         opacity: 0,
+    //     }
+    //     // opacity: visible ? 1 : 0,
+    //     // y: visible ? 0 : 24
+    // });
+
+    return transitions(({opacity}, item) => {
+        console.log(item);
+        console.log(opacity);
+        return (
+            <>
+                <animated.div>{children}</animated.div>
+            </>
+        )
+    });
+};
 
 const App = () => {
-    console.log('App');
+
+
     return (
-        <>
-            <Toggle>
-                {({ visible, onToggle }) => (
-                    <>
-                        <Modal visible={visible} onToggle={onToggle}>
+        <UserProvider>
+            {/* <User /> */}
+            <section>
+                <Toggle>
+                    {({ visible, onToggle }) => (
+                        <>
+                            {visible && (
+                                <FadeIn visible={visible}>show me</FadeIn>
+                            )}
+                            {
+                                transitions
+                            }
+                            {/* {visible && <animated.h1 style={styles}>show me</animated.h1>} */}
+                            {/* <Modal visible={visible} onToggle={onToggle}>
                             <h1>111111111111</h1>
-                        </Modal>
-                        <button onClick={onToggle}>Open</button>
-                    </>
-                )}
-            </Toggle>
-        </>
+                        </Modal> */}
+                            <button onClick={onToggle}>Show / Hide</button>
+                        </>
+                    )}
+                </Toggle>
+            </section>
+        </UserProvider>
     );
 };
 
